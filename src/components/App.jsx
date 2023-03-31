898c21e65012fe1b577667b88575cb5a89acfc95
Natalia Karpenko <106306905+natalia-no-war@users.noreply.github.com>
2023-03-31 10:10:39 +0300

    without redux-presist

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,70 @@
+import { useSelector } from 'react-redux';
+
+import { FormInput } from './Form/FormInput';
+import { ContactsList } from './ContactsList/ContactsList';
+import { getContacts } from 'redux/selectors';
+import { Filter } from './Filter/Filter';
+
 export const App = () => {
+  const contacts = useSelector(getContacts);
+  // const [contacts, setContacts] = useState(() => {
+  //   return JSON.parse(localStorage.getItem('contacts')) ?? [];
+  // });
+
+  // const [filter, setFilter] = useState('');
+
+  // useEffect(() => {
+  //   localStorage.setItem('contacts', JSON.stringify(contacts));
+  // }, [contacts]);
+
+  // const formSubmitHandler = contactInfo => {
+  //   if (contacts.some(({ name }) => name === contactInfo.name)) {
+  //     alert(`${contactInfo.name} is already in contacts`);
+  //     return;
+  //   }
+
+  //   const finalContact = {
+  //     id: nanoid(),
+  //     ...contactInfo,
+  //   };
+
+  //   setContacts(prevContacts => [finalContact, ...prevContacts]);
+  //   return true;
+  // };
+
+  // const contactDeleteHandler = contactId => {
+  //   setContacts(prevContacts =>
+  //     prevContacts.filter(contact => contact.id !== contactId)
+  //   );
+  // };
+
+  // const contactFilterHandler = event => {
+  //   setFilter(event.target.value);
+  // };
+
+  // const getFilteredContacts = () => {
+  //   const normalizedFilter = filter.toLowerCase();
+  //   return contacts.filter(({ name }) =>
+  //     name.toLowerCase().includes(normalizedFilter)
+  //   );
+  // };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <h1>Phonebook</h1>
+      {/* <FormInput onFormSubmit={formSubmitHandler} /> */}
+      <FormInput />
+
+      {/* <Filter value={filter} contactFilter={contactFilterHandler} /> */}
+      <Filter />
+
+      {contacts.length !== 0 && (
+        <ContactsList
+        // onContactDelete={contactDeleteHandler}
+        // contacts={getFilteredContacts()}
+        //
+        />
+      )}
+    </>
   );
 };
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+
+import { FilterStyled } from './Filter.styled';
+import { setFilter } from 'redux/filterSlice';
+
+export const Filter = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <FilterStyled>
+      Find contacts by name
+      <input
+        type="text"
+        placeholder="Enter a name"
+        onChange={e => dispatch(setFilter(e.target.value))}
+      />
+    </FilterStyled>
+  );
+};
+
+Filter.propTypes = {
+  value: PropTypes.string.isRequired,
+  contactFilter: PropTypes.func.isRequired,
+};
